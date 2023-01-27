e8604bd38980f6ac9796e90e155ae044e9a4fab3
Daegyeom Ha <combbm@gmail.com>
2023-01-27 18:18:58 +0900

    chore: update content option

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./packages/**/app/**/*.{js,ts,jsx,tsx}', './packages/**/components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   // eslint-disable-next-line global-require
   plugins: [require('@tailwindcss/line-clamp')]
 };
