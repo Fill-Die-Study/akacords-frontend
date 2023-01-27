9b7c7db52004c4eb43c887e328a04a16fc744773
Daegyeom Ha <combbm@gmail.com>
2023-01-27 18:15:13 +0900

    feat(service): create global.css

--- packages/service/app/layout.tsx
+++ packages/service/app/layout.tsx
@@ -1,14 +1,11 @@
-import './globals.css';
+import '@styles/global.css';
+import React from 'react';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang="ko">
       <head />
-      <body>{children}</body>
+      <body className="box-border">{children}</body>
     </html>
   );
 }
