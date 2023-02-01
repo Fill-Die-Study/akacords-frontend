b405aef2c972d744068baa874f37e0fddb70b64a
한동진 <arkc1009@gmail.com>
2023-02-01 18:02:18 +0900

    chore: add primary-color(temp) to tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,14 @@
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   // eslint-disable-next-line global-require
-  plugins: [require('@tailwindcss/line-clamp')]
+  plugins: [require('@tailwindcss/line-clamp')],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          500: '#0A0D14'
+        }
+      }
+    }
+  }
 };
