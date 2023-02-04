bec639706d2032c439a3b3baadec2d91c461686a
Daegyeom Ha <combbm@gmail.com>
2023-02-04 21:06:43 +0900

    feat(service): add form event usign react-hook-form

--- packages/service/components/LoginNextButton/index.tsx
+++ packages/service/components/LoginNextButton/index.tsx
@@ -4,11 +4,12 @@ import React, { PropsWithChildren } from 'react';
 
 interface LoginNextButtonProps {
   readonly className?: string;
+  readonly onClick?: (e: React.MouseEvent) => void;
 }
 
-export default function LoginNextButton({ children, className }: PropsWithChildren<LoginNextButtonProps>) {
+export default function LoginNextButton({ children, className, onClick }: PropsWithChildren<LoginNextButtonProps>) {
   return (
-    <button type="button" className={`w-80 h-[4.3rem] rounded text-xl ${className}`}>
+    <button type="button" className={`w-80 h-[4.3rem] rounded text-xl ${className}`} onClick={onClick}>
       {children}
     </button>
   );
