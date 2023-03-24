332c9271dddf02b794c635fb7bbe2e5894dfa895
Daegyeom Ha <combbm@gmail.com>
2023-03-24 23:57:44 +0900

    fix(ui): fix position bug of label

--- packages/ui/src/components/Input/Input.tsx
+++ packages/ui/src/components/Input/Input.tsx
@@ -7,8 +7,8 @@ export interface InputProps {
 
 export function Input({ value, label }: InputProps) {
   return (
-    <div className="w-[348px] p-[14px_13px] flex items-center gap-[10px] border border-[#C4C6CC] rounded-[8px] bg-white">
-      {label && <span className="h-[14px] text-[12px] mt-[-15%] bg-white px-1">{label}</span>}
+    <div className="relative w-[348px] p-[14px_13px] flex items-center gap-[10px] border border-[#C4C6CC] rounded-[8px] bg-white">
+      {label && <span className="absolute h-[14px] text-[12px] mt-[-15%] bg-white px-1">{label}</span>}
       <input
         className="w-[231px] px-[10px] text-sm leading-[17px] font-medium placeholder:text-[#AAADB3] outline-none"
         type="text"
