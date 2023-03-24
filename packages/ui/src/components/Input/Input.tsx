bdbd3b8de1d3ce53a040c6fb2aa4bd9be15952db
Daegyeom Ha <combbm@gmail.com>
2023-03-25 01:04:42 +0900

    feat(ui): change label and placeholder according to state

--- packages/ui/src/components/Input/Input.tsx
+++ packages/ui/src/components/Input/Input.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useRef, useState } from 'react';
 
 export interface InputProps {
   value: string;
   label: string;
+  placeholder: string;
 }
 
-export function Input({ value, label }: InputProps) {
+export function Input({ value, label, placeholder }: InputProps) {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [isInputFocused, setInputFocused] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      if (isInputFocused) {
+        inputRef.current.placeholder = placeholder;
+      } else {
+        inputRef.current.placeholder = label;
+      }
+    }
+  }, [isInputFocused, placeholder, label]);
+
   return (
-    <div className="relative w-[348px] p-[14px_13px] flex items-center gap-[10px] border border-[#C4C6CC] rounded-[8px] bg-white">
-      {label && <span className="absolute h-[14px] text-[12px] mt-[-15%] bg-white px-1">{label}</span>}
+    <div className="relative w-[348px] p-[14px_13px] flex items-center gap-[10px] border border-[#C4C6CC] rounded-[8px] bg-white focus-within:border-black">
       <input
-        className="w-[231px] px-[10px] text-sm leading-[17px] font-medium placeholder:text-[#AAADB3] outline-none"
+        ref={inputRef}
+        onFocus={() => setInputFocused(true)}
+        onBlur={() => setInputFocused(false)}
+        className="peer w-[231px] px-[10px] text-sm leading-[17px] font-medium placeholder:text-[#AAADB3] outline-none"
         type="text"
+        placeholder={placeholder}
         value={value}
       />
+      <span className="hidden absolute h-[14px] text-[12px] mt-[-15%] bg-white px-1 text-[#AAADB3] peer-focus:text-black peer-focus:inline peer-[:not(:placeholder-shown)]:inline">
+        {label}
+      </span>
     </div>
   );
 }
