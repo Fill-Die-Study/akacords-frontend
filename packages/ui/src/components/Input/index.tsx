a0265e6151239bb4143dcd6fd02ce30f9bed3b80
Daegyeom Ha <combbm@gmail.com>
2023-03-25 01:36:17 +0900

    feat(ui): add disabled option

--- packages/ui/src/components/Input/index.tsx
+++ packages/ui/src/components/Input/index.tsx
@@ -8,11 +8,13 @@ export interface InputProps {
   label: string;
   placeholder: string;
   status?: InputStatus;
+  disabled?: boolean;
 }
 
-export function Input({ value, label, placeholder, status = InputStatus.DEFAULT }: InputProps) {
+export function Input({ value, label, placeholder, status = InputStatus.DEFAULT, disabled = false }: InputProps) {
   const inputRef = useRef<HTMLInputElement | null>(null);
   const [isInputFocused, setInputFocused] = useState<boolean>(false);
+  const disabledStyle = disabled ? 'bg-[#E1E4E8] text-[#AAADB3] cursor-not-allowed' : '';
 
   useEffect(() => {
     if (inputRef.current) {
@@ -26,16 +28,17 @@ export function Input({ value, label, placeholder, status = InputStatus.DEFAULT 
 
   return (
     <div
-      className={`relative w-[348px] p-[14px_13px] flex items-center gap-[10px] border border-[#C4C6CC] rounded-[8px] bg-white ${InputBorderColorMap[status]}`}
+      className={`${disabledStyle} relative w-[348px] p-[14px_13px] flex items-center gap-[10px] border border-[#C4C6CC] rounded-[8px] bg-white ${InputBorderColorMap[status]}`}
     >
       <input
         ref={inputRef}
         onFocus={() => setInputFocused(true)}
         onBlur={() => setInputFocused(false)}
-        className="peer w-[231px] px-[10px] text-sm leading-[17px] font-medium placeholder:text-[#AAADB3] outline-none"
+        className={`${disabledStyle} peer w-[231px] px-[10px] text-sm leading-[17px] font-medium placeholder:text-[#AAADB3] outline-none`}
         type="text"
         placeholder={placeholder}
         value={value}
+        disabled={disabled}
       />
       <span
         className={`hidden absolute h-[14px] text-[12px] mt-[-15%] bg-white px-1 text-[#AAADB3] ${LabelTextColorMap[status]} peer-focus:inline peer-[:not(:placeholder-shown)]:inline`}
