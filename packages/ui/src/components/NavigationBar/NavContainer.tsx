f09f44b2ed43dd3f40c1d648ef2707568d4bab36
Daegyeom Ha <combbm@gmail.com>
2023-06-09 17:27:04 +0900

    fix(ui): add z-index and absolute styles

--- packages/ui/src/components/NavigationBar/NavContainer.tsx
+++ packages/ui/src/components/NavigationBar/NavContainer.tsx
@@ -1,5 +1,9 @@
 import React, { PropsWithChildren } from 'react';
 
 export function NavContainer({ children }: PropsWithChildren) {
-  return <div className="w-full h-[110px] bg-transparent flex items-center justify-between px-[230px]">{children}</div>;
+  return (
+    <div className="z-10 absolute w-full h-[110px] bg-transparent flex items-center justify-between px-[230px]">
+      {children}
+    </div>
+  );
 }
