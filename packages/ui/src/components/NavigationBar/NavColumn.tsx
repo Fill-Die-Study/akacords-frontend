708c8d5af3677e66a8e0fae59dab77447c39b080
Daegyeom Ha <combbm@gmail.com>
2023-06-16 18:33:17 +0900

    style: fix typo in NavColumn component
    Co-authored-by: arkc1009 <52207750+arkc1009@users.noreply.github.com>

--- packages/ui/src/components/NavigationBar/NavColumn.tsx
+++ packages/ui/src/components/NavigationBar/NavColumn.tsx
@@ -1,11 +1,11 @@
 import React, { PropsWithChildren } from 'react';
 
-interface NavContainerProps {
+interface NavColumnProps {
   readonly isNarrow?: boolean;
   readonly className?: string;
 }
 
-export function NavColumn({ isNarrow, className, children }: PropsWithChildren<NavContainerProps>) {
+export function NavColumn({ isNarrow, className, children }: PropsWithChildren<NavColumnProps>) {
   const space = isNarrow ? 'space-x-[30px]' : 'space-x-14';
   return <div className={`flex items-center ${space} ${className}`}>{children}</div>;
 }
