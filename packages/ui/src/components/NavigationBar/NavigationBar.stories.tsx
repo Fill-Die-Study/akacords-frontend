a6c3f0b40464b6aab1a330533d2a4e50235e4c9c
Daegyeom Ha <combbm@gmail.com>
2023-08-01 18:02:15 +0900

    fix(ui, storybook): remove unnecessary `use client`

--- packages/ui/src/components/NavigationBar/NavigationBar.stories.tsx
+++ packages/ui/src/components/NavigationBar/NavigationBar.stories.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { NavColumn, NavContainer, NavItem, NavText } from '@src/components/NavigationBar';
